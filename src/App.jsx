24203998f6f7804a4957fedcd0d596d934b7d779
Anurag Bansal <devanuragbansal@gmail.com>
2025-04-15 05:32:37 +0530

    Add vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import FillInTheBlanksPage from "./pages/FillInTheBlanksPage";
 import ResultPage from "./pages/ResultPage";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/quiz" element={<FillInTheBlanksPage />} />
-      <Route path="/result" element={<ResultPage />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/quiz" element={<FillInTheBlanksPage />} />
+        <Route path="/result" element={<ResultPage />} />
+      </Routes>
+      <Analytics />
+    </>
   );
 }
 
